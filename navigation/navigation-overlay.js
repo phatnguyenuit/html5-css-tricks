8737c9a8a7b22181091c1e8fb7fbe3659309b130
Nguyen Phat <phatnt.uit@gmail.com>
2020-02-27 23:05:11 +0700

    Initial HTML5 tricks

---
+++ navigation/navigation-overlay.js
@@ -0,0 +1,9 @@
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById('my-sidebar').style.width = '250px';
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById('my-sidebar').style.width = '0';
+}
